66e7b360af249aa6fe20e3a8c05883474abd839c
Mark Vodyanitskiy <mvodya@gmail.com>
2020-02-25 23:35:01 +1000

    + еще куча типов файловых систем

--- operation-systems/lab1/partstypes.h
+++ operation-systems/lab1/partstypes.h
@@ -136,11 +136,134 @@ void print_type(unsigned char type) {
     case 0x63:
       printf("GNU HURD or Sys");
       break;
+    case 0x64:
+      printf("Novell Netware");
+      break;
+    case 0x65:
+      printf("Novell Netware");
+      break;
+    case 0x70:
+      printf("DiskSecure Mult");
+      break;
+    case 0x75:
+      printf("PC/IX");
+      break;
+    case 0x80:
+      printf("Old Minix");
+      break;
+    case 0x81:
+      printf("Minix");
+      break;
     case 0x82:
       printf("Linux swap");
       break;
     case 0x83:
       printf("Linux");
+      break;
+    case 0x84:
+      printf("OS/2 hidden C:");
+      break;
+    case 0x85:
+      printf("Linux extended");
+      break;
+    case 0x86:
+      printf("NTFS volume set");
+      break;
+    case 0x87:
+      printf("NTFS volume set");
+      break;
+    case 0x8e:
+      printf("Linux LVM");
+      break;
+    case 0x93:
+      printf("Amoeba");
+      break;
+    case 0x94:
+      printf("Amoeba BBT");
+      break;
+    case 0x9f:
+      printf("BSD/OS");
+      break;
+    case 0xa0:
+      printf("IBM Thinkpad");
+      break;
+    case 0xa5:
+      printf("BSD/386");
+      break;
+    case 0xa6:
+      printf("OpenBSD");
+      break;
+    case 0xa7:
+      printf("NeXTSTEP");
+      break;
+    case 0xb7:
+      printf("BSDI fs");
+      break;
+    case 0xb8:
+      printf("BSDI swap ");
+      break;
+    case 0xbb:
+      printf("Boot Wizard hid");
+      break;
+    case 0xc1:
+      printf("DRDOS/sec");
+      break;
+    case 0xc4:
+      printf("DRDOS/sec");
+      break;
+    case 0xc6:
+      printf("DRDOS/sec");
+      break;
+    case 0xc7:
+      printf("Syrinx");
+      break;
+    case 0xda:
+      printf("Non-FS data");
+      break;
+    case 0xdb:
+      printf("CP/M / CTOS");
+      break;
+    case 0xde:
+      printf("Dell Utility");
+      break;
+    case 0xdf:
+      printf("BootIt");
+      break;
+    case 0xe1:
+      printf("DOS access");
+      break;
+    case 0xe3:
+      printf("DOS R/O");
+      break;
+    case 0xe4:
+      printf("SpeedStor");
+      break;
+    case 0xeb:
+      printf("BeOS fs");
+      break;
+    case 0xee:
+      printf("EFI GPT");
+      break;
+    case 0xef:
+      printf("EFI (FAT-12/16/32)");
+      break;
+    case 0xf1:
+      printf("SpeedStor");
+      break;
+    case 0xf4:
+      printf("SpeedStor");
+      break;
+    case 0xf2:
+      printf("DOS secondary");
+      break;
+    case 0xfd:
+      printf("Linux raid auto");
+      break;
+    case 0xfe:
+      printf("LANstep");
+      break;
+    case 0xff:
+      printf("BBT");
       break;
     default:
       printf("unknow %02x", type);
