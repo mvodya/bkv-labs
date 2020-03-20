2f979b0b2057d64a5460bd47aa58da8f42863bda
Mark Vodyanitskiy <mvodya@gmail.com>
2020-03-20 19:45:00 +1000

    Comments

--- computer-architecture/task7/task7.asm
+++ computer-architecture/task7/task7.asm
@@ -1,8 +1,6 @@
 ; Task7 for computer architecture subject
 ; Fibonacci series
-; by Mark Vodyanitskiy
-
-; WORK IN PROGRESS
+; by Mark Vodyanitskiy, Elena Bova
 
 section .text
 global _start
@@ -10,18 +8,20 @@ global _start
 _start:
   mov eax, 1
   mov ebx, 1
-  ; print first two 1
+  ; Print first two 1
   call print
   call print
 
 fibbo_loop:
-  mov ecx, eax
-  add ecx, ebx
-  jc exit
+  mov ecx, eax          ; set first number
+  add ecx, ebx          ; add second number
+  jc exit               ; check overflow
   push eax
   mov eax, ecx
+  ; Print number
   call print
   pop eax
+  ; Swap numbers
   mov eax, ebx
   mov ebx, ecx
   jmp fibbo_loop
@@ -42,21 +42,21 @@ print:
 
   mov esi, 10
   mov ebx, str + 20
-  xor ecx, ecx ; size (clear)
-  inc ecx ; for new line print
+  xor ecx, ecx          ; size (clear)
+  inc ecx               ; for new line print
 
-print_div_loop:
+print_div_loop:         ; convert number to ascii char array
   xor edx, edx
   div esi
-  add edx, 48 ; add ascii offset (for character '0')
+  add edx, 48           ; add ascii offset (for character '0')
   
   dec ebx
-  mov [ebx], dl ; reverse save
-  inc ecx ; size counter
+  mov [ebx], dl         ; reverse save
+  inc ecx               ; size counter
   cmp eax, 0
   jnz print_div_loop
 
-  ; print number
+  ; Print number
   mov edx, ecx
   mov ecx, ebx
   mov ebx, 1
@@ -73,4 +73,4 @@ print_div_loop:
 
 
 section .data
-str: times 21 db 0Ah
+str: times 21 db 0Ah    ; char array buffer for printing
