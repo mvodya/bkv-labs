b0ee093908d156336d25cce5d91a5c7cb3dd77fb
Mark Vodyanitskiy <mvodya@gmail.com>
2020-03-23 15:03:42 +1000

    Лабораторная 8 (в процессе)

---
+++ computer-architecture/task8/task8.asm
@@ -0,0 +1,93 @@
+; by Mark Vodyanitskiy, Elena Bova
+; Q(n) = Q(n - Q(n - 1)) + Q(n - Q(n - 2)), n > 2
+;                  B                 A
+
+; 0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15  16  17  18  19
+; 1, 1, 2, 3, 3, 4, 5, 5, 6, 6, 6, 8, 8, 8, 10, 9, 10, 11, 11, 12
+; A  B  v  v  v  v  v  v  v  x   !
+; 1  1  x                    7
+; 1, 1, 2, 3, 3, 4, 5, 5, 6, 6, 6, 8, 8, 8, 10, 9, 10,
+; 1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16  17
+
+section .text
+global qnumbers
+extern n 
+
+qnumbers:
+  mov rsi, dword 2
+qloop:
+
+  mov rax, rsi
+  call qcalc
+  mov rcx, rax
+  mov rax, rsi
+  call qgetpointer
+  mov [rax], rcx  
+
+  inc rsi
+  cmp rsi, 19
+  je qloop_exit
+  jmp qloop
+
+qloop_exit:
+  mov rbx, n
+  mov [rbx], rcx
+  ret
+
+qcalc:
+  ; input: rax - n
+  ; output rax - Q(n)
+  ; Q(n) = Q(n - Q(n - 1)) + Q(n - Q(n - 2)), n > 2
+  ;                  B                 A
+  push rsi
+  push rcx
+  mov rsi, rax
+
+  ; PART B
+  mov rcx, rsi 
+  dec rax
+  call qgetpointer
+  sub rcx, [rax]
+  mov rax, rcx
+  call qgetpointer
+  mov rcx, [rax]
+  push rcx
+  
+
+  ; PART A
+  mov rcx, rsi
+  mov rax, rsi
+  dec rax
+  dec rax
+  call qgetpointer
+  sub rcx, [rax]
+  mov rax, rcx
+  call qgetpointer
+  mov rcx, [rax]
+
+  ; Summation B + A
+  pop rax ; get rcx
+  add rax, rcx
+
+  pop rcx
+  pop rsi
+  ret
+
+qgetpointer:
+  ; input: rax - n
+  ; output: rax - pointer to digits
+  push rsi
+  push rbx
+  mov rsi, s
+  mov rbx, dword 8
+  mul rbx
+  add rax, rsi
+
+  pop rbx
+  pop rsi
+  ret
+
+section .data
+s: dq 1, 1
+digits: times 3000040 dq 0
+;nums for calculating and output
---
+++ computer-architecture/task8/task8.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+unsigned long long int n = 0;
+
+int main() {
+  
+  qnumbers();
+  printf("n: %lld\n", n);
+
+  
+
+  return 0;
+}
