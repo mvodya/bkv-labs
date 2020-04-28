b90cb0b4017ea89ac7ea59cefbf4c6879c48314b
Mark Vodyanitskiy <mvodya@gmail.com>
2020-04-28 12:19:10 +1000

    Fix to no-pie

--- computer-architecture/task11/mk.sh
+++ computer-architecture/task11/mk.sh
@@ -1,7 +1,7 @@
 nasm -felf64 Sin.asm
 if [ $? -eq 0 ]
  then
-  g++  sin.cxx Sin.o Cartesian.o -dl -lfltk -lX11 -oSin
+  g++ -no-pie sin.cxx Sin.o Cartesian.o -dl -lfltk -oSin -g
   if [ $? -eq 0 ]
    then
      ./Sin
